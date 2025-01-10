431ce90a7dfd67ec694ca6f5f046c08fa929edcf
David NYIRINGABO <nyiringabodavid62@gmail.com>
2025-01-10 20:27:56 +0200

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by David NYIRINGABO on 1/10/25.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
